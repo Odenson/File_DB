4609bda31c0ffd1ef330d1ecfcadcc30d0d769aa
Gary Febb <gary.febbrarino@gmail.com>
2023-06-22 16:22:48 +1000

    merge error on version 2 new methods and ID

--- class_cvs_database.py
+++ class_cvs_database.py
@@ -1,6 +1,6 @@
 import csv
 import os
-
+import uuid
 
 class DataTable:
     def __init__(self, table_name, headers=None):
@@ -11,16 +11,45 @@ class DataTable:
         self.table_name = table_name
         self.headers = headers if headers else []
         self.rows = []
-        
+        self.table_location = self.get_table_location()
+
+        # Ensure "ID" heading is the first column
+        self.ensure_id_heading()
+
         # Check if the CSV file exists
-        if os.path.isfile(f"{table_name}.csv"):
+        if self.table_exists():
             self.load()  # Load data from the existing file
+
+    def get_table_location(self):
+        """
+        Get the file path of the table.
+        """
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        return os.path.join(current_directory, f"{self.table_name}.csv")
+
+    def table_exists(self):
+        """
+        Check if the CSV file exists for the table.
+        """
+        return os.path.isfile(self.table_location)
+            
+    def ensure_id_heading(self):
+        """
+        Ensure "ID" heading is the first column of the headers list.
+        If "ID" is already present, move it to the first position.
+        If "ID" is not present, add it as the first header.
+        """
+        if "ID" in self.headers:
+            self.headers.remove("ID")  # Remove "ID" if already present
+        self.headers.insert(0, "ID")  # Add "ID" as the first header
 
     def add(self, row):
         """
-        Add a new row to the table.
+        Add a new row to the table with a unique ID.
         """
-        self.rows.append(row)
+        record_id = str(uuid.uuid4())  # Generate a unique ID
+        row_with_id = [record_id] + row
+        self.rows.append(row_with_id)
 
     def delete(self, index):
         """
@@ -28,6 +57,18 @@ class DataTable:
         """
         if index < len(self.rows):
             del self.rows[index]
+
+    def amend(self, row_id, amended_row):
+        """
+        Amend an existing row in the table using the row's ID.
+        If the incorrect ID is provided, raise an error indicating that the record does not exist.
+        """
+        for i, row in enumerate(self.rows):
+            if row[0] == row_id:
+                self.rows[i] = [row_id] + amended_row
+                break
+        else:
+            raise ValueError("Record does not exist")
 
     def search(self, field, value):
         """
@@ -46,7 +87,7 @@ class DataTable:
         """
         Save changes to the CSV file.
         """
-        with open(f"{self.table_name}.csv", "w", newline="") as file:
+        with open(f"{self.table_location}", "w", newline="") as file:
             writer = csv.writer(file)
             writer.writerow(self.headers)  # Write the header row
             writer.writerows(self.rows)  # Write the data rows
@@ -55,7 +96,13 @@ class DataTable:
         """
         Load data from the CSV file.
         """
-        with open(f"{self.table_name}.csv", "r") as file:
+        with open(f"{self.table_location}", "r") as file:
             reader = csv.reader(file)
             self.headers = next(reader)  # Read the header row
             self.rows = list(reader)  # Read the data rows
+
+    def list_all(self):
+        """
+        List all records in the current table.
+        """
+        return self.rows
--- test_csv_db_class.py
+++ test_csv_db_class.py
@@ -1,17 +1,36 @@
-from File_DB.class_cvs_database import DataTable
+from class_cvs_database import DataTable
 
-# create a new table with headers
+# create a new table with headers, this will only add heading if file of this database does not exit (ie new db)
 table = DataTable("employees", ["name", "age", "position"])
 
 # add rows to the table
-table.add(["John Doe", 30, "Manager"])
-table.add(["Jane Smith", 25, "Assistant"])
-table.add(["Jane Doe", 30, "Legend"])
-table.add(["Gary Febb", 51, "Assistant"])
+#table.add(["John Doe", 30, "Manager"])
+#table.add(["Jane Smith", 25, "Assistant"])
+#table.add(["Jane Doe", 30, "Legend"])
+#table.add(["Gary Febb", 51, "Assistant"])
+#table.add(["Giordan Febb", 19, "Student"])
 
+
+'''#amend an existing row
+try:
+    id = "d3ceb69c-40ef-4e4a-a1b7-7e680ad62de"
+    table.amend(id, ["Gary Febb", 51, "Managing Partner"])
+    print("Row updated for ID:" + id)
+except:
+    print("Row not found for id: "+id)
+'''
+    
 # save changes to the file
 table.save()
 
 # search for all employees with age 30
-results = table.search("age", 30)
-print(results)
+results = table.search("age", "30")
+print("search result")
+for row in results:
+    print(row)
+
+
+#list all records in Table
+#records = table.list_all()
+#for row in records:
+#    print(row)
